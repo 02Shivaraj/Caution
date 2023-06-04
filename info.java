79da5c4f4df30af6891dd822958bb8a40a6aae20
02Shivaraj <135525133+02Shivaraj@users.noreply.github.com>
2023-06-04 20:06:23 +0530

    Update info.java

--- info.java
+++ info.java
@@ -3,7 +3,7 @@ class info{
         int f1=0,f2=1,f3;
         System.out.println(f1);
         System.out.println(f2);
-        int a=10;
+        int a=15;
         for(int i=0;i<a;i++){
             f3=f1+f2;
             System.out.println(f3);
