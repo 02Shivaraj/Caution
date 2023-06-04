52333050d0fd9ad4b4cc6565174ddb56658ce17e
02Shivaraj <shivaraj.a.2611@gmail.com>
2023-06-04 20:03:43 +0530

    1st Commit

---
+++ info.java
@@ -0,0 +1,14 @@
+class info{
+    public static void main(String[] args) {
+        int f1=0,f2=1,f3;
+        System.out.println(f1);
+        System.out.println(f2);
+        int a=10;
+        for(int i=0;i<a;i++){
+            f3=f1+f2;
+            System.out.println(f3);
+            f1=f2;
+            f2=f3;
+        }
+    }
+}
